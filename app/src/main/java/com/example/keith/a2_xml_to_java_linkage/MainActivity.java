90add1992a2ba5dcdf20cff9bcabfb7e381cc7ca
Keith <kperkins411@gmail.com>
2018-01-18 14:39:37 -0500

    Initial commit

---
+++ app/src/main/java/com/example/keith/a2_xml_to_java_linkage/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.keith.a2_xml_to_java_linkage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button but2;
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        but2 = (Button)findViewById(R.id.button2);
+        tv = (TextView)findViewById(R.id.textView);
+
+        but2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("                 Clicked by button2");
+            }
+        });
+
+    }
+
+    public void doButton1(View view) {
+        tv.setText("Clicked by button1");
+    }
+}
