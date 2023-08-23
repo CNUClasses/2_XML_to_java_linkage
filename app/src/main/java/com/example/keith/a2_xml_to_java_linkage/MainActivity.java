a9ff2d840882a9c5053ccb9653ee45da28b9c345
Keith Perkins <keith.perkins@cnu.edu>
2023-08-23 11:17:29 -0400

    Few changes

--- app/src/main/java/com/example/keith/a2_xml_to_java_linkage/MainActivity.java
+++ app/src/main/java/com/example/keith/a2_xml_to_java_linkage/MainActivity.java
@@ -9,28 +9,35 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    Button but2;
     TextView tv;
+    Button but2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        but2 = (Button)findViewById(R.id.button2);
-        tv = (TextView)findViewById(R.id.textView);
-
+        tv = findViewById(R.id.textView);
+        but2=findViewById(R.id.button2);
         but2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                tv.setText("                 Clicked by button2");
+                tv.setText("Button 2 clicked");
             }
         });
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
     }
 
     public void doButton1(View view) {
-        tv.setText("Clicked by button1");
+        tv.setText("Button 1 clicked");
     }
+
 }
